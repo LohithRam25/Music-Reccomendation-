2ac77c235ba499a44b826231cb3e409929bc1921
ram-25082005 <rampalla2005@gmail.com>
2025-03-19 22:42:06 +0530

    ..

---
+++ application/app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import pickle
+# Create your views here.
+from django.http import HttpResponse
+def home(request):
+    if request.method=="POST":
+        music_input=request.POST["song_name"]
+        print(music_input)
+        with open("D:\\MACHINE LEARNING\\PROJECTS\\Music Reccomendation\\model.pkl", "rb") as model1:
+            model = pickle.load(model1)
+        with open("D:\\MACHINE LEARNING\\PROJECTS\\Music Reccomendation\\vectors.pkl", "rb") as vector1:
+            vectors = pickle.load(vector1)
+        with open("D:\\MACHINE LEARNING\\PROJECTS\\Music Reccomendation\\data.pkl", "rb") as data1:
+            data = pickle.load(data1)
+
+        hai = data.index[data["track_name"].str.lower() == music_input.lower()].tolist()
+
+        if hai:
+            distances, indices = model.kneighbors(vectors[hai[0]].reshape(1, -1))
+            reccomendations=[]
+            for i in indices.flatten():
+                reccomendations.append([data["track_name"].iloc[i],data["track_artist"].iloc[i]])
+            return render(request,"home.html",{"reccomendations":reccomendations})
+        else:
+            msg="Sorry!!! We Cannot Find the songs That are similar to the song.. Kindly check the spelling of the song You have entered or try another song"
+            return render(request, "home.html", {"msg": msg})
+
+
+
+
+        #for i in indices.flatten():
+          #  print(data["track_name"].iloc[i] + " " + data["track_artist"].iloc[i])
+        #return render(request,"home.html")
+    else:
+        return render(request, "home.html")
